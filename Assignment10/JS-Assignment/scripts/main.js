9e4de4d8fe58200ae1ed7f46bbdc8583e7ffc527
Rachel Stuckey <rachelstuckey@Rachels-MacBook-Pro.local>
2016-11-05 12:05:19 -0500

    Assignment 10 - JavaScript

--- Assignment10/JS-Assignment/scripts/main.js
+++ Assignment10/JS-Assignment/scripts/main.js
@@ -1,3 +1,5 @@
+var array = [];
+
 function update() {
     //////////////////////Your Code Starts here
 
@@ -7,37 +9,58 @@ function update() {
     var lastName = getValue('lastName');
     console.log(firstName);
     console.log(lastName);
+    
+    var fullName = firstName + " " + lastName;
+    
+    getElm('p1_fullName').innerHTML = fullName;
 
-
-
+    if (firstName != "" && lastName != "") {
+        getElm("p1_valid").innerHTML = true;
+    } else {
+        getElm("p1_valid").innerHTML = false;
+    }
+    
+    getElm("p1_fullNameLength").innerHTML = fullName.length;
 
     // Part 2
     var emailAddress = getValue('emailAddress');
     console.log(emailAddress);
-
-
+    
+    getElm('p2_email').innerHTML = emailAddress;
+    
+    
+    if (emailAddress.indexOf('@') > -1) {
+        getElm('p2_valid').innerHTML = true;
+    } else {
+        getElm('p2_valid').innerHTML = false;
+    }
+    
+    getElm('p2_emailLength').innerHTML = emailAddress.length;
+    
     // Part 3
     var randomNumber = getValue('randomNumber');
     console.log(randomNumber);
-
-
-
+    
+    getElm('p3_number').innerHTML = randomNumber;
+    
+    if (parseInt(randomNumber) > -1) {
+        getElm('p3_valid').innerHTML = true;
+    } else {
+        getElm('p3_valid').innerHTML = false;
+    }
 
     // Part 4
     var arrayInput = getValue('arrayValue');
     console.log(arrayInput);
-
-
-
-
-
-
-
-
-
-
-
-    ////////////////////////Youre code ends here.
+    
+    array.push(arrayInput);
+    
+    getElm('p4_valid').innerHTML = array;
+    
+    getElm('p4_arraylength').innerHTML = array.length;
+    
+    
+    ////////////////////////Your code ends here.
 
 }
 
