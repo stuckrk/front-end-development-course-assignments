61d8550e4eb3e65d8c561dae3f02ba9fcba668b6
Rachel Stuckey <rachelstuckey@yahoo.com>
2016-12-06 19:30:09 -0600

    Assignment 17

--- Assignment17/scripts/main.js
+++ Assignment17/scripts/main.js
@@ -1,5 +1,42 @@
 $(document).ready(function(){
-
-
+    $('.error').hide();
+    
+    var inputGroups = $('.input-group');
+    
+    evenItems = inputGroups.filter(':even');
+    
+    evenItems.addClass('even');
+    
+    $('#firstName').on('blur', errorMessage);
+    $('#lastName').on('blur', errorMessage);
+    $('#emailAddress').on('blur', errorEmail);
+    
+    function errorMessage() {
+        var input = $(this);
+        
+        if(input.val() === ''){
+            input.closest('div').find('span').fadeIn();
+        }else{
+            input.closest('div').find('span').fadeOut();
+        }
+    }
+    
+    
+    function errorEmail() {
+        var input = $(this);
+        
+        if(input.val() === ''){
+            input.closest('div').find('.requiredMsg').fadeIn();
+        }else{
+            input.closest('div').find('.requiredMsg').fadeOut();
+        }
+        
+        if(input.val().indexOf('@') === -1 ||           input.val().indexOf('.com') === -1){
+            input.closest('div').find('.emailMsg').fadeIn();
+        }else if(input.val()){
+            input.closest('div').find('.emailMsg').fadeOut();
+        }
+    }
+    
 
 });
